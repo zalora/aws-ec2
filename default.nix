cf43b1ca8efe93609967f33f22e350a707e66559
Vladimir Kirillov <proger@wilab.org.ua>
2014-11-20 11:17:33 +0200

    nix: use shallow-fetchgit and make put-metric a static binary

--- default.nix
+++ default.nix
@@ -1,21 +1,71 @@
 let
-   nixpkgs = (import <nixpkgs> {}).fetchgit {
-    url = "git://github.com/NixOS/nixpkgs.git";
-    rev = "e6ac094dc5c3470571292744c4b007d7da40256f";
-    sha256 = "1dcd4cc2c769f285cbd278ff35222ff49d0e764d7043f843a4e90e870b82af40";
+  unsafeDerivation = args: derivation ({
+    PATH = builtins.getEnv "PATH";
+    system = builtins.currentSystem;
+    builder = ./bootstrap/proxy-bash.sh;
+    preferLocalBuild = true;
+    __noChroot = true;
+  } // args);
+
+  shell = name: command: unsafeDerivation {
+    inherit name;
+    args = ["-c" command];
+  };
+
+  # working git must be in $PATH.
+  # increase (change) `clock' to trigger updates
+  shallow-fetchgit =
+    {url, branch ? "master", clock ? 1}:
+      shell "${baseNameOf (toString url)}-${toString clock}" ''
+        git clone --depth 1 -b ${branch} --recursive ${url} $out
+        cd $out
+      '';
+
+  nixpkgs = shallow-fetchgit {
+    url = "git://github.com/zalora/nixpkgs.git";
+    branch = "upcast";
+    clock = 1;
   };
 in
 { system ? builtins.currentSystem
 , pkgs ? import nixpkgs { inherit system; }
 , name ? "aws-ec2"
 , src ? builtins.filterSource (path: type: type != "unknown" && baseNameOf path != ".git" && baseNameOf path != "result" && baseNameOf path != "dist") ./.
-, haskellPackages ? pkgs.haskellPackages_ghc782
+, haskellPackages ? pkgs.haskellPackages_ghc783
 }:
+with pkgs.lib;
 
-haskellPackages.buildLocalCabalWithArgs {
+let
+  # Build a cabal package given a local .cabal file
+  buildLocalCabalWithArgs = { src
+                            , name
+                            , args ? {}
+                            , cabalDrvArgs ? { jailbreak = true; }
+                            # for import-from-derivation, want to use current system
+                            , nativePkgs ? import pkgs.path {}
+                            }: let
+
+    cabalExpr = shell "${name}.nix" ''
+      export HOME="$TMPDIR"
+      ${let c2n = builtins.getEnv "OVERRIDE_cabal2nix";
+        in if c2n != "" then c2n
+           else "${nativePkgs.haskellPackages.cabal2nix}/bin/cabal2nix"} \
+        ${src + "/${name}.cabal"} --sha256=FILTERME \
+          | grep -v FILTERME | sed \
+            -e 's/{ cabal/{ cabal, cabalInstall, cabalDrvArgs ? {}, src/' \
+            -e 's/cabal.mkDerivation (self: {/cabal.mkDerivation (self: cabalDrvArgs \/\/ {/' \
+            -e 's/buildDepends = \[/buildDepends = \[ cabalInstall/' \
+            -e 's/pname = \([^$]*\)/pname = \1  inherit src;/'  > $out
+    '';
+  in haskellPackages.callPackage cabalExpr ({ inherit src cabalDrvArgs; } // args);
+
+  aws = haskellPackages.callPackage ./aws.nix {};
+in
+
+buildLocalCabalWithArgs {
   inherit src name;
   args = {
-    aws = haskellPackages.callPackage ./aws.nix {};
+    inherit aws;
   };
 }
 
